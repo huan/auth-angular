c92f04e0982f05858fa2cbdb5576da51ccd530f5
Huan LI <zixia@zixia.net>
2018-05-07 15:57:01 +0800

    add ts-node

--- scripts/package-publish-config-tag-next.ts
+++ scripts/package-publish-config-tag-next.ts
@@ -6,7 +6,10 @@ const PACKAGE_JSON = path.join(__dirname, '../package.json')
 
 const pkg = require(PACKAGE_JSON)
 
-pkg.publishConfig.tag = 'next'
+pkg.publishConfig = {
+  ...(pkg.publishConfig || {}),
+  tag: 'next',
+}
 
 fs.writeFileSync(PACKAGE_JSON, JSON.stringify(pkg, null, 2))
 // console.log(JSON.stringify(pkg, null, 2))
